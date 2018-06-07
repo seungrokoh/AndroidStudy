1db72f649d18d0757ff059e6f0f13ae627122222
davidoh <devdavidoh@gmail.com>
2018-06-07 19:14:19 +0900

    firestore first commit

---
+++ RecyclerVIewExample/app/src/main/java/com/example/davidoh/recyclerviewexample/DataService.kt
@@ -0,0 +1,18 @@
+package com.example.davidoh.recyclerviewexample
+
+object DataService {
+
+    val contentData = arrayListOf(
+            Content("image1", "blabla1", "https://pixabay.com/photo-3444212/"),
+            Content("image2", "blabla1", "https://pixabay.com/photo-690685/"),
+            Content("image3", "blabla1", "https://pixabay.com/photo-1337986/"),
+            Content("image4", "blabla1", "https://pixabay.com/photo-1109588/"),
+            Content("image5", "blabla1", "https://pixabay.com/photo-3186483/"),
+            Content("image6", "blabla1", "https://pixabay.com/photo-2925250/"),
+            Content("image7", "blabla1", "https://pixabay.com/photo-3385966/"),
+            Content("image8", "blabla1", "https://pixabay.com/photo-868715/"),
+            Content("image9", "blabla1", "https://pixabay.com/photo-1072828/"),
+            Content("image10", "blabla1", "https://pixabay.com/photo-1072828/")
+
+    )
+}
--- RecyclerVIewExample/app/src/main/java/com/example/davidoh/recyclerviewexample/MainActivity.kt
+++ RecyclerVIewExample/app/src/main/java/com/example/davidoh/recyclerviewexample/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.davidoh.recyclerviewexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var adapter = ContentRecyclerAdapter(this, DataService.contentData)
+        recyclerView.adapter = adapter
+
+        val recyclerLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = recyclerLayoutManager
+        recyclerView.setHasFixedSize(true)
+
     }
 
-    class MainRecy
 }
