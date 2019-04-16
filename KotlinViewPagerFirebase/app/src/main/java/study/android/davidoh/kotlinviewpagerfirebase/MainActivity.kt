2f22fe0be7f1cd2ec98d231043facb31ab4d130a
seungrokoh <devdavidoh@gmail.com>
2019-04-16 20:04:05 +0900

    add : create kotlin-viewpager-firebase

---
+++ KotlinViewPagerFirebase/app/src/main/java/study/android/davidoh/kotlinviewpagerfirebase/MainActivity.kt
@@ -0,0 +1,12 @@
+package study.android.davidoh.kotlinviewpagerfirebase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
